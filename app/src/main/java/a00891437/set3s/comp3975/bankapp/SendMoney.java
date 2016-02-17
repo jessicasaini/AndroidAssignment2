4f08ea189acf53af132704d57db8ce29157c26b6
Jessica <jessicasaini@hotmail.com>
2016-02-16 23:12:13 -0800

    adding all files

---
+++ app/src/main/java/a00891437/set3s/comp3975/bankapp/SendMoney.java
@@ -0,0 +1,44 @@
+package a00891437.set3s.comp3975.bankapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class SendMoney extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_money);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_send_money, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    public void onClickHomeSendMoney(final View view){
+        Intent i = new Intent(this, MyRightDrawer.class);
+        startActivity(i);
+    }
+
+}
